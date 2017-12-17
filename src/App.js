a2577014493523456b796621cd009d0b0b06e597
Thiago Dallacqua <thiagodallacqua@outlook.com>
2017-12-17 19:19:19 -0300

    removed the routing control state property

--- src/App.js
+++ src/App.js
@@ -6,12 +6,6 @@ import './App.css'
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
      bookshelfs: [//I've createt a state to store the bookshelfs of a user, which contains the name of the bookshelf and the books of each one
        {
          shelfTitle: 'Currently Reading',
@@ -63,8 +57,7 @@ class BooksApp extends React.Component {
            },
          ],
        },
-     ],
-     showSearchPage: false
+     ]
   }
 
   render() {
